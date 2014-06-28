0485a59126e9b88e020df29429751ca46cfe5f99
Phyks <webmaster@phyks.me>
2014-06-28 23:32:43 +0200

    Updated demo code

--- Software/RaspberryPi/receive.cpp
+++ Software/RaspberryPi/receive.cpp
@@ -2,8 +2,9 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <time>
-#include "./RF24.h"
+#include <ctime>
+#include <mysql.h>
+#include "../RF24.h"
 
 const bool DEBUG = true;
 
@@ -14,52 +15,51 @@ const rf24_datarate_e NRF_SPEED = RF24_1MBPS;
 
 // PreAmplifier level for the nRF
 // Lower this to reduce power consumption. This will reduce range.
-const rf24_pa_dbm_e NRF_PA_LEVEL = RF24_PA_MAX;
+const rf24_pa_dbm_e NRF_PA_LEVEL = RF24_PA_LOW;
 
 // Channel for the nrf module
 // 76 is default safe channel in RF24
-const int NRF_CHANNEL = 76;
+const int NRF_CHANNEL = 0x4c;
 
-const uint64_t pipe = 0xE056D446D0LL;
+const uint64_t addr = 0xF0F0F0F0E1LL;
 
 // Struct to receive RF data
 typedef struct {
     int power;
 } PayloadTX;
 
-RF24 radio(RPI_V2_GPIO_P1_15, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ);
-
-void handlePayload(PayloadTX payload) {
-    if(DEBUG) {
-        time(&timer);
-
-        std::cout << "[" << difftime(start, timer) << "]" << "Packet received.\n";
-        std::cout << "Power : " << payload.power << "\t";
-    }
-}
+//RF24 radio(RPI_V2_GPIO_P1_15, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ);
+RF24 radio("/dev/spidev0.0",8000000 , 25);
 
 int main() {
-    PayloadTX payload = {0, 0, 0};
+    PayloadTX payload = {0};
     time_t start = time(NULL);
-    time_t timer;
+MYSQL mysql;
+mysql_init(&mysql);
+mysql_options(&mysql, MYSQL_READ_DEFAULT_GROUP,"option");
+
+if(!mysql_real_connect(&mysql, "localhost", "USER", "PASSWORD", "citizenwatt", 0, NULL, 0)) {
+printf("Erreur lors de la connexion à la BDD");
+exit(1);
+}
 
     // Initialize nRF
     radio.begin();
-    radio.setChannel(NRF_CHANNEL);
     // Max number of retries and max delay between them
     radio.setRetries(15, 15);
+    radio.setChannel(NRF_CHANNEL);
     // Reduce payload size to improve reliability
-    radio.setPayloadSize(8);
+    //radio.setPayloadSize(8);
     // Set the datarate
-    radio.setDataRate(NRF_SPEED);
+    //radio.setDataRate(NRF_SPEED);
     // Use the largest CRC
-    radio.setCRCLength(RF24_CRC_16);
+    //radio.setCRCLength(RF24_CRC_16);
     // Ensure auto ACK is enabled
-    radio.setAutoAck(1);
+    //radio.setAutoAck(1);
     // Use the best PA level
     radio.setPALevel(NRF_PA_LEVEL);
     // Open reading pipe
-    radio.openReadingPipe(pipe);
+    radio.openReadingPipe(1, addr);
 
     radio.startListening();
 
@@ -67,7 +67,14 @@ int main() {
         if(radio.available()) {
             radio.read(&payload, sizeof(PayloadTX));
 
-            handlePayload(payload);
+	    if(DEBUG) {
+		//std::cout << "[" << difftime(start, timer) << "]" << "Packet received.\n";
+		std::cout << "Power : " << payload.power << "\n";
+	    }
+
+	    char request[150] = "";
+	    sprintf(request, "INSERT INTO measures(id, date, power) VALUES('', NOW(), %d)", payload.power);
+	    mysql_query(&mysql, request);
         }
     }
 }
